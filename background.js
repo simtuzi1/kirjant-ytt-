0008fa6ecc33c0340ba83a42c005a285b843d9d8
simtuzi1 <87931516+simtuzi1@users.noreply.github.com>
2023-03-23 08:40:10 +0200

    Keybindings

---
+++ background.js
@@ -0,0 +1,29 @@
+chrome.commands.onCommand.addListener((command) => {
+
+    if (command === 'open-popup-window')
+        chrome.system.display.getInfo({ singleUnified: true }, (info) => {
+        
+          const wDimension = info[0].workArea;
+          const { top, left, height, width } = wDimension;
+          console.log(top);
+          const w = 440;
+          const h = 220;
+          const l = width / 2 - w / 2 + left;
+          const t = height / 2 - h / 2 + top;
+          const newWindow = () => {
+            console.log('in new window function');
+          };
+          chrome.windows.create(
+            {
+              url: 'popup.html',
+              type: 'popup',
+              width: w,
+              height: h,
+              left: Math.round(l),
+              top: Math.round(t),
+            },
+            newWindow
+          );
+        });
+    }
+)
